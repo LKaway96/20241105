3658b65154878522543629902782f01ff71e3061
LKaway96 <dreamataiwan@gmail.com>
2024-11-05 15:31:06 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,22 @@
+let font;  //載入字型文字
+let points = [];  //轉成點狀文字
+let angle = 0
+let r = 5
+// ==================================================
+function preload(){ 
+    font = loadFont("fonts/NotoSansTC-VariableFont_wght.ttf") 
+}
+//===================================================
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("#81b29a");
+  angleMode(DEGREES) //設定三角函數的角度用0~360度
+  background("#CAD2C5")
+//===================================================
+  points = font.textToPoints("TKUET", -300, 80, 200, {
+    sampleFactor:0.06
+  }); //轉成文字圖檔，放在(0, 200)位置，圖形大小為200，sampleFactor為點數距離大小。值越小代表點數越小                                                              
 }
 
 function draw() {
@@ -11,6 +27,23 @@ function draw() {
 
   let rectWidth = 50 + mouseX / 20; 
   let circleSize = 50 + mouseY / 20;
+
+  translate(width/2,height/2) //把原本原點(0,0)在左上角，改到畫布中心點
+
+  rotate(frameCount%360) // 360旋轉
+
+  for (let i=0; i<points.length-1; i++) { 
+    //text(str(i),points[i].x,points[i].y) //在(points[i].x,points[i].y)座標上，顯示一個文字
+    fill("#59C3C3")
+    //noStroke()
+    strokeWeight(1)
+    ellipse(points[i].x+r*sin(angle+i*15),points[i].y,10) //+i*15 所有點不同搖晃
+    stroke("#52489C")
+    strokeWeight(3)
+    //line(points[i].x,points[i].y,points[i+1].x,points[i+1].y) //畫線，兩個點，第一個點(points[i].x,points[i].y)，第二個點(points[i+1].x,points[i+1].y)
+ } 
+
+    angle = angle+10
 
   for (let j = 0; j < 20; j++) {
     for (let i = 0; i < 40; i++) {
